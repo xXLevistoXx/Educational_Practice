10c10e1872fc9370383b63e763bd9b27b5ae5bda
Uteshin Lev Dmitrievich <uteshinlev5@gmail.com>
2024-10-06 14:10:12 +0300

    Практическая_4_Init

--- school/src/main/java/com/web_project/school/repository/EmployeeRepository.java
+++ school/src/main/java/com/web_project/school/repository/EmployeeRepository.java
@@ -8,7 +8,13 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface EmployeeRepository extends JpaRepository<EmployeeModel, Long> {
-    @Query("SELECT e FROM EmployeeModel e WHERE e.deleted = false AND e.Name LIKE %:value% OR e.FirstName LIKE %:value% OR e.LastName LIKE %:value% OR e.CorpEmail LIKE %:value% OR e.Position LIKE %:value%")
+    @Query("SELECT e FROM EmployeeModel e WHERE " +
+            "e.deleted = false AND " +
+            "e.Name LIKE %:value% " +
+            "OR e.FirstName LIKE %:value% " +
+            "OR e.LastName LIKE %:value% OR " +
+            "e.CorpEmail LIKE %:value% OR " +
+            "e.Position LIKE %:value%")
     List<EmployeeModel> searchByAnyField(@Param("value") String value);
 
     @Query("SELECT e FROM EmployeeModel e WHERE e.deleted = false")
